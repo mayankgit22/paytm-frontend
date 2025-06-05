797eeaf6b630b4e47c45a31174a2542b2a3a657b
mayank kumar <162879224+mayankgit22@users.noreply.github.com>
2025-06-05 16:40:51 +0530

    Update DashBoard.jsx

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -158,7 +158,7 @@ function Card({ Name, picUrl,id }) {
   <div className="fixed top-5 right-5 bg-green-500 text-white px-4 py-2 rounded shadow-lg z-50">
     ✅ ₹{amount} sent successfully! to {Name}
   </div>
-)}
+
 
       )}
     </>
